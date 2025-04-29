5f4abd4560b345385289e5f7b4799e153987aa9e
h23mlars <h23mlars@du.se>
2025-04-29 14:35:43 +0200

    Rens

--- supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Ersätt med dina Supabase-uppgifter som du får från Supabase Dashboard
-const supabaseUrl = 'https://owiszgzxvfcmldeawhpf.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93aXN6Z3p4dmZjbWxkZWF3aHBmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2OTY5MTYsImV4cCI6MjA1OTI3MjkxNn0.l1QOPd9zR5rgeJvpPs1cZx-A1Ly2kPLRY-IXqt_soFA';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
--- trafikdata-api/trafikDataService.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-
-async function getTrafficData() {
-    const response = await fetch('https://api.trafikverket.se/v1/traffic-data-endpoint');
-    const data = await response.json();
-    console.log(data);
-}
-
-getTrafficData();
